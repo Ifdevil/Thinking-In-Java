1dd561600bc38bd2a681632c7a64503f3d91dc63
ifdevil <384867771@qq.com>
2019-01-09 21:38:17 +0800

    Atomic

---
+++ src/thinkingInJava/chapter21/AtomicityTest.java
@@ -0,0 +1,36 @@
+package thinkingInJava.chapter21;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 原子性
+ */
+public class AtomicityTest implements Runnable{
+
+    private int i = 0;
+    public int getValue(){
+        return i;
+    }
+    public synchronized void evenIncrement(){
+        ++i;++i;
+    }
+
+    @Override
+    public void run() {
+        evenIncrement();
+    }
+
+    public static void main(String[] args) {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        AtomicityTest at = new AtomicityTest();
+        exec.execute(at);
+        while (true){
+            int val = at.getValue();
+            if(val % 2 != 0){
+                System.out.println(val);
+                System.exit(0);
+            }
+        }
+    }
+}
