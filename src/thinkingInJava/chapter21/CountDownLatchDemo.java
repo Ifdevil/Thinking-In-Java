c1131911bacc99002319c04fef06b08742f47b52
ifdevil <384867771@qq.com>
2019-01-15 23:06:10 +0800

    CountDownLatch

--- src/thinkingInJava/chapter21/CountDownLatchDemo.java
+++ src/thinkingInJava/chapter21/CountDownLatchDemo.java
@@ -1,4 +1,72 @@
 package thinkingInJava.chapter21;
 
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * CountDownLatch
+ */
 public class CountDownLatchDemo {
+    static final int SIZE = 100;
+    public static void main(String[] args) {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        CountDownLatch latch = new CountDownLatch(SIZE);
+        for (int i = 0; i < 10; i++) {
+            exec.execute(new WaitingTask(latch));
+        }
+        for (int i = 0; i < SIZE; i++) {
+            exec.execute(new TaskPortion(latch));
+        }
+        System.out.println("Launced all tasks");
+        exec.shutdown();
+    }
+
+}
+class TaskPortion implements Runnable{
+    private static int counter = 0;
+    private final int id = counter++;
+    private static Random random = new Random(47);
+    private final CountDownLatch latch;
+    TaskPortion(CountDownLatch latch){
+        this.latch = latch;
+    }
+    @Override
+    public void run() {
+        try {
+            doWork();
+            latch.countDown();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    public void doWork() throws InterruptedException {
+        TimeUnit.MILLISECONDS.sleep(random.nextInt(2000));
+        System.out.println(this+"completed");
+    }
+    public String toString(){
+        return String.format("%1$-3d",id);
+    }
+}
+class WaitingTask implements Runnable{
+    private static int counter = 0;
+    private final int id = counter++;
+    private final CountDownLatch latch;
+    WaitingTask(CountDownLatch latch){
+        this.latch = latch;
+    }
+    @Override
+    public void run() {
+        try {
+            latch.await();
+            System.out.println("Latch barrier passed for "+this);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    public String toString(){
+        return String.format("WaitingTask %1$-3d ",id);
+    }
 }
